b7e95c8bceaa19a7c981dcf5e0a093bf3bc3aeff
Vladyslav Kovalchuk <prostoandrei9@gmail.com>
2025-06-29 14:40:23 +0300

    disable usdc

--- src/pages/create/page.tsx
+++ src/pages/create/page.tsx
@@ -445,7 +445,7 @@ export default function CreatePayment() {
                     className="w-24 h-10 mt-5 px-3 py-2 bg-neutral-800 border border-neutral-700 rounded-md text-white"
                   >
                     <option value="ETH">ETH</option>
-                    <option value="USDC">USDC</option>
+                    {/* <option value="USDC">USDC</option> */}
                   </select>
                 </div>
 
